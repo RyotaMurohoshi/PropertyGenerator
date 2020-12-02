a2901b7196c6b9e40339586dbb485db6d7bbad12
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2020-12-03 00:25:49 +0900

    init sample project

---
+++ PropertyGenerator.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PropertyGenerator.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
